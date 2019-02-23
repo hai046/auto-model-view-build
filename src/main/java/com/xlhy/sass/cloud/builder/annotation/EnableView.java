61fdebbf2697b3fa869ed01312081be1e152e287
denghaizhu <denghaizhu@xianlai-inc.com>
2019-02-23 10:58:00 +0800

    add source

---
+++ src/main/java/com/xlhy/sass/cloud/builder/annotation/EnableView.java
@@ -0,0 +1,19 @@
+package com.xlhy.sass.cloud.builder.annotation;
+
+import com.xlhy.sass.cloud.builder.configuration.ViewBuilderScannerRegistrar;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * @author denghaizhu
+ * @date 2019-02-22
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import(ViewBuilderScannerRegistrar.class)
+public @interface EnableView {
+
+    String[] viewBuilderPackages();
+}
---
+++ src/main/java/com/xlhy/sass/cloud/builder/model/HasId.java
@@ -0,0 +1,9 @@
+package com.xlhy.sass.cloud.builder.model;
+
+/**
+ * @author denghaizhu
+ * @date 2019-02-22
+ */
+public interface HasId<T> {
+    T getId();
+}
